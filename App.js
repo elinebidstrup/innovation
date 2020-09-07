e802dcced5a7dce744cfc8f3220a7e8dfaa2ed8e
elinebidstrup <elbi18ac@student.cbs.dk>
2020-09-07 17:37:45 +0200

    init

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TitleComponent from "./Components/TitleComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hej Eline</Text>
       <StatusBar style="auto" />
+      <TitleComponent title = 'Første title'/>
+      <TitleComponent title = 'Anden title'/>
     </View>
   );
 }
